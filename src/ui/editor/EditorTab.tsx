44461578305a1dbdaefb788863c61ce1502a824d
Pfuster12 <p.fuster.a@gmail.com>
2019-11-19 08:42:12 +0000

    Styled editor tabs.

--- src/ui/editor/EditorTab.tsx
+++ src/ui/editor/EditorTab.tsx
@@ -3,18 +3,35 @@ import { FileIcon } from '../sidebar/plugins/folders/FileIcon'
 import CloseIcon from '../components/CloseIcon'
 
 interface EditorTabProps {
-    tab: string
+    tab: string,
+    selected: string,
+    onClick: (tab: string) => void
+    onCloseClick: (tab: string) => void
 }
 
 /**
  * Displays an Editor Tab of an open file.
  */
 export default function EditorTab(props: EditorTabProps) {
+
+    /**
+     * Handles Tab click.
+     * @param event 
+     */
+    function onCloseClick(event: React.SyntheticEvent) {
+        props.onCloseClick(props.tab)
+    }
+
+    function onClick(event: React.SyntheticEvent) {
+        props.onClick(props.tab)
+    }
+
     return (
-        <div className="editor-tab">
+        <div className={props.tab === props.selected ? "editor-tab editor-tab-selected" : "editor-tab"}
+            onClick={onClick}>
             <FileIcon filename={props.tab}/>
             <span className="editor-tab-name">{props.tab}</span>
-            <CloseIcon fill="#000"/>
+            <CloseIcon onClick={onCloseClick} class="editor-tab-close"/>
         </div>
     )
 }
--- src/ui/editor/EditorTabLayout.tsx
+++ src/ui/editor/EditorTabLayout.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState } from 'react'
 import EditorTab from './EditorTab'
 
 interface EditorTabLayoutProps {
@@ -10,10 +11,34 @@ interface EditorTabLayoutProps {
  */
 export default function EditorTabLayout(props: EditorTabLayoutProps) {
 
+    /**
+     * Store the selected tab index.
+     */
+    const [currentTab, setCurrentTab] = useState(props.tabs[0])
+
+    /**
+     * Handle a tab onClick.
+     * @param event
+     */
+    function onTabCloseClick(tab: string) {
+    }
+
+    /**
+     * Handle a tab onClick.
+     * @param event
+     */
+    function onTabClick(tab: string) {
+        setCurrentTab(tab)
+    }
+
     return (
         <nav className="editor-tablayout">
             {
-                props.tabs.map(tab => <EditorTab tab={tab}/>)
+                props.tabs.map(tab => <EditorTab onClick={onTabClick}
+                    onCloseClick={onTabCloseClick} 
+                    key={tab}
+                    selected={currentTab}
+                    tab={tab}/>)
             }
         </nav>
     )
