7d20d619845cd68b1967a0767713018799be5d10
Pfuster12 <p.fuster.a@gmail.com>
2019-12-10 17:59:27 +0000

    improvement(statusbar): added line count to statusbar

--- src/ui/editor/StatusBar.tsx
+++ src/ui/editor/StatusBar.tsx
@@ -2,9 +2,10 @@ import * as React from 'react'
 import { useMemo } from 'react'
 
 export default function StatusBar() {
+    const lineCount = 0
     return (
         <div className="status-bar">
-
+            <span>Ln {lineCount}</span>
         </div>
     )
 }
